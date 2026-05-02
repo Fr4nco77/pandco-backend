5296c6e0f2ed24b31f107133c3251182ac0ad316
Franco Carreras <fcarreras777@gmail.com>
2026-05-02 12:34:07 -0300

    add throttler and helmet middleware, update dependencies in package.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Una vez terminado hay que restringir el origin y metodos
+  app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Fatal error during bootstrap:');
+  console.error(err);
+  process.exit(1);
+});
