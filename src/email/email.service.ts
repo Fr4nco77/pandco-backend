d519f121a18b19d78038e0c4b3a67d9d2a7dd258
Franco Carreras <fcarreras777@gmail.com>
2026-05-03 23:20:57 -0300

    add EmailModule and EmailService with email sending functionality

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -12,13 +12,13 @@ interface EmailContent {
 }
 
 @Injectable()
-export class ResendService {
-  private resend: Resend;
+export class EmailService {
+  private mailer: Resend;
   private emailFrom: string;
   private frontendUrl: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.resend = new Resend(this.configService.get<string>('RESEND_KEY'));
+    this.mailer = new Resend(this.configService.get<string>('RESEND_KEY'));
     this.emailFrom = this.configService.get<string>(
       'MAIL_FROM',
       'onboarding@resend.dev',
@@ -31,7 +31,7 @@ export class ResendService {
 
   private async sendEmail(to: string, { subject, html }: EmailContent) {
     try {
-      const { data, error } = await this.resend.emails.send({
+      const { data, error } = await this.mailer.emails.send({
         from: this.emailFrom,
         to,
         subject,
@@ -45,28 +45,28 @@ export class ResendService {
 
       return data;
     } catch (err: any) {
-      console.error('Resend Error:', err);
+      console.error('Mailer Error:', err);
       throw new InternalServerErrorException('Email service unavailable');
     }
   }
 
   async sendEmailRegister(to: string, name: string) {
     const template = registerTemplate(name, this.frontendUrl);
-    return await this.sendEmail(to, template);
+    await this.sendEmail(to, template);
   }
 
   async sendEmailForgotPassword(to: string, name: string, token: string) {
     const template = forgotPasswordTemplate(name, this.frontendUrl, token);
-    return this.sendEmail(to, template);
+    await this.sendEmail(to, template);
   }
 
   async sendEmailPurchase(to: string, name: string) {
     const template = purchaseTemplate(name, this.frontendUrl);
-    return this.sendEmail(to, template);
+    await this.sendEmail(to, template);
   }
 
   async sendEmailPurchaseError(to: string, name: string) {
     const template = purchaseErrorTemplate(name, this.frontendUrl);
-    return this.sendEmail(to, template);
+    await this.sendEmail(to, template);
   }
 }
